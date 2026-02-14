de317f1e9b38b5c47d9a897e752d0b1febd2d445
Anmol <91004363+anmolx@users.noreply.github.com>
2026-02-14 22:50:04 +0530

    Update TextFooter.tsx

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -13,7 +13,7 @@ export default function TextFooter() {
       <h1
         className={`absolute left-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight ${playfairDisplay.className}`}
       >
-      <span className="text-[#CEA2FD]"> <br /> Niharika Ji</span> <br /> match the photo pairs
+      <span className="text-[#CEA2FD]"> <br /> Match</span> <br /> the photo pairs
       </h1> 
 
       {/* Right Text */}
